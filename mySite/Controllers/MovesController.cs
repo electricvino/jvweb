26ea5354d8ad9d929bc243d35e868c4a04bea724
Jon Vinson <jvinson@spp.org>
2014-08-20 07:59:57 -0500

    syncing with server.

---
+++ mySite/Controllers/MovesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mySite.Controllers
+{
+    public class MovesController : ApiController
+    {
+        private string clientId = "AsIe08Xk7v0jVsnh81zJ3zeZN2Y4g230";
+        private string clientSecret = "BO8Sg5mf039cAV57RYb3262K4o2fKZ0gVxH3CR2GRW3bu6j0eyQ9x_ws22efwawL";
+        // GET: api/Moves
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Moves/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
